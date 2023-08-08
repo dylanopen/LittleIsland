365dca04421ea0c3f0c8ce441611e6465601ce54
Dylan Rogers <opendylan@proton.me>
2023-08-08 15:50:04 +0100

    0.0.2

--- src/world/Generation.java
+++ src/world/Generation.java
@@ -1,17 +1,10 @@
 package world;
 
-import config.Data;
-import draw.Window;
-
 public class Generation
 {
 
 	public static World genIsland()
 	{
-		
-		Window w = Data.window;
-
-		w.canvas.
 		
 		return new World();
 		
